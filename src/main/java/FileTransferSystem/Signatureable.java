f9595614dd043677280ddb8c0535e01433dea599
TaiguRekan <dremlmarina@gmail.com>
2024-12-04 16:52:00 +0300

    Delete src/main directory

--- src/main/java/FileTransferSystem/Signatureable.java
+++
@@ -1,6 +0,0 @@
-package FileTransferSystem;
-
-public interface Signatureable {
-    byte[] sign(byte[] data) throws Exception;
-    boolean verify(byte[] data, byte[] signature) throws Exception;
-}
