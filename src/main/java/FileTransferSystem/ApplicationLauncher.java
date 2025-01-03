dc2dffa850b90a87b0beb3e9594fa4cf72996b54
TaiguRekan <dremlmarina@gmail.com>
2025-01-03 05:26:53 +0300

    Update ApplicationLauncher.java

--- src/main/java/FileTransferSystem/ApplicationLauncher.java
+++ src/main/java/FileTransferSystem/ApplicationLauncher.java
@@ -2,7 +2,16 @@ package FileTransferSystem;
 
 import javax.swing.*;
 
+/**
+ * Класс для запуска приложения передачи файлов.
+ */
 public class ApplicationLauncher {
+    
+    /**
+     * Главный метод, который запускает приложение.
+     * Пользователь выбирает режим работы: запуск сервера, клиента или обоих одновременно.
+     * @param args Аргументы командной строки.
+     */
     public static void main(String[] args) {
         String[] options = {"Запустить сервер", "Запустить клиент", "Запустить клиент и сервис одновременно"};
         int choice = JOptionPane.showOptionDialog(null, "Выберите режим запуска:", "Файл передатчик",
